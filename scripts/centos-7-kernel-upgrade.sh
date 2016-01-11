28d7e35726a99849b0df31ecd3bd3a6628ece969
Sub <subchen@gmail.com>
2016-01-11 16:31:22 +0800

    Update centos-7-kernel-upgrade.sh

--- scripts/centos-7-kernel-upgrade.sh
+++ scripts/centos-7-kernel-upgrade.sh
@@ -4,10 +4,10 @@ rpm -import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 
 # kernel-lt（lt=long-term）
-yum –enablerepo=elrepo-kernel install -y kernel-lt
+# yum –enablerepo=elrepo-kernel install -y kernel-lt
 
 # kernel-ml（ml=mainline）
-# yum –enablerepo=elrepo-kernel install -y kernel-ml
+yum --enablerepo=elrepo-kernel install -y kernel-ml
 
 echo
 echo "Please reload your host to take effect."
