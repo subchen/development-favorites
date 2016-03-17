a34cd9621c7194c8206d1cc1801c814f393cf8f7
Sub <subchen@gmail.com>
2016-03-17 11:29:29 +0800

    Create oracle-jdk-download.sh

---
+++ scripts/oracle-jdk-download.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+JDK_VERSION=8u73
+JDK_VERSION_PATCH=b02
+
+curl -fSL \
+  http://download.oracle.com/otn-pub/java/jdk/${JDK_VERSION}-${JDK_VERSION_PATCH}/jdk-${JDK_VERSION}-linux-x64.rpm \
+  -H "Cookie: oraclelicense=accept-securebackup-cookie" \
+  -o jdk-${JDK_VERSION}-linux-x64.rpm
