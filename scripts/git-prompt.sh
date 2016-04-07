3bff49ce720574ff3cf75c0aa064f4e801c600c9
Sub <subchen@gmail.com>
2016-04-07 12:51:05 +0800

    Create git-prompt.sh

---
+++ scripts/git-prompt.sh
@@ -0,0 +1,12 @@
+url https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+
+cat >> ~/.bashrc << EOF
+source ~/.git-prompt.sh
+
+MAGENTA_COLOR=$'\e[1;35m'
+NORMAL_COLOR=$'\e[m'
+export GIT_PS1_SHOWDIRTYSTATE=1
+export PS1='[\u@\h \w]$MAGENTA_COLOR$(__git_ps1)$NORMAL_COLOR# '
+EOF
+
+source ~/.bashrc
