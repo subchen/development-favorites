60b6193df86134bf818b7951ad0ef6eff80d3918
Sub <subchen@gmail.com>
2016-04-07 13:06:17 +0800

    Update git-prompt.sh

--- scripts/git-prompt.sh
+++ scripts/git-prompt.sh
@@ -3,12 +3,11 @@
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 
 cat >> ~/.bashrc << EOF
-source ~/.git-prompt.sh
 
-MAGENTA_COLOR=$'\e[1;35m'
-NORMAL_COLOR=$'\e[m'
+# git branch on bash prompt
+source ~/.git-prompt.sh
 export GIT_PS1_SHOWDIRTYSTATE=1
-export PS1='[\u@\h \w]$MAGENTA_COLOR$(__git_ps1)$NORMAL_COLOR# '
+export PS1='[\\u@\\h \\w]\\e[1;35m\$(__git_ps1)\\e[m# '
 EOF
 
 source ~/.bashrc
