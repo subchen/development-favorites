f86965ae9a68b3dac51ec382dc612f1e3e1efec7
Sub <subchen@gmail.com>
2016-05-11 12:38:52 +0800

    Create npm-proxy.sh

---
+++ scripts/npm-proxy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat > /usr/bin/npm-tb << EOF
+#!/bin/bash
+exec npm --registry=https://registry.npm.taobao.org $*
+EOF
+
+chmod +x /usr/bin/npm-tb
+
+cat > /usr/bin/npm-cn << EOF
+#!/bin/bash
+exec npm --registry=http://registry.cnpmjs.org $*
+EOF
+
+chmod +x /usr/bin/npm-cn
