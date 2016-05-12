4152567a8a30affb8b65c6f0c66b176517aeda2c
Sub <subchen@gmail.com>
2016-05-12 22:09:45 +0800

    Update centos-7-base.sh

--- scripts/centos-7-base.sh
+++ scripts/centos-7-base.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ `id -u` != 0 ]; then
-  echo "Please run this script under root"
-  exit 1
-fi
-
 yum install -y git vim zip unzip tree net-tools psmisc bind-utils
 
 systemctl disable iptables-services firewalld
@@ -21,59 +16,3 @@ cat >> /etc/environment << EOF
 LANG=en_US.UTF-8
 LC_ALL=en_US.UTF-8
 EOF
-
-cat > ~/.vimrc << EOF
-filetype on
-syntax on
-set encoding=utf-8
-set background=light
-set tabstop=4
-set shiftwidth=4
-set number
-set expandtab
-set showmatch
-set ruler
-set ignorecase
-set hlsearch
-set incsearch
-set modeline
-set showcmd
-set showfulltag
-set showmode
-set smartcase
-set imcmdline
-
-" no auto comments on next line
-set formatoptions-=c formatoptions-=r formatoptions-=o
-
-" highlight current line
-set cursorline
-hi CursorLine cterm=NONE ctermbg=brown ctermfg=black
-
-" highlight trailing whitespace
-hi TrailingWhitespace ctermbg=red
-match TrailingWhitespace /\s\+$/
-EOF
-
-cat > ~/.gitconfig << EOF
-[push]
-    default = simple
-[user]
-    name = Guoqiang Chen
-    email = subchen@gmail.com
-[color]
-    ui = true
-EOF
-
-
-curl -fSL https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-
-cat >> ~/.bashrc << EOF
-# git branch on bash prompt
-source ~/.git-prompt.sh
-export GIT_PS1_SHOWDIRTYSTATE=1
-export PS1='[\\u@\\h \\w]\\e[1;35m\$(__git_ps1)\\e[m# '
-EOF
-
-source ~/.bashrc
-
