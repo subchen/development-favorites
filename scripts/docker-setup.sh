c6599fa08f454c27b77a61771694b138a7c4cb9d
Sub <subchen@gmail.com>
2016-05-12 22:29:54 +0800

    Update docker-setup.sh

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-curl -fSL https://get.docker.com/builds/Linux/x86_64/docker-1.9.1 -o /usr/bin/docker
-curl -fSL https://github.com/docker/compose/releases/download/1.5.2/docker-compose-Linux-x86_64 -o /usr/bin/docker-compose
-curl -fSL https://github.com/docker/machine/releases/download/v0.5.5/docker-machine_linux-amd64 -o /usr/bin/docker-machine
-chmod +x /usr/bin/docker*
+curl -fSL https://get.docker.com/builds/Linux/x86_64/docker-1.11.1.tgz -o /tmp/docker.tgz
+tar -zxvf /tmp/docker.tgz --strip-components=1 -C /usr/bin/
+
+curl -fSL https://github.com/docker/compose/releases/download/1.7.1/docker-compose-Linux-x86_64 -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+
+curl -fSL https://github.com/docker/machine/releases/download/v0.7.0/docker-machine_linux-amd64 -o /usr/bin/docker-machine
+chmod +x /usr/bin/docker-machine
 
 cat > /etc/systemd/system/multi-user.target.wants/docker.service << EOF
 [Unit]
@@ -12,9 +16,9 @@ Documentation=https://docs.docker.com
 After=network.target
 
 [Service]
-EnvironmentFile=/etc/sysconfig/docker
-ExecStart=/usr/bin/docker daemon -H unix:///var/run/docker.sock \$DOCKER_OPTS
 Type=notify
+EnvironmentFile=/etc/sysconfig/docker
+ExecStart=/usr/bin/docker daemon -H 0.0.0.0:2376 -H unix:///var/run/docker.sock \$DOCKER_OPTS
 MountFlags=slave
 LimitNOFILE=1048576
 LimitNPROC=1048576
@@ -27,11 +31,8 @@ WantedBy=multi-user.target
 EOF
 
 cat > /etc/sysconfig/docker << EOF
-DOCKER_OPTS="-H 0.0.0.0:2376 --bip=10.8.0.1/24"
+DOCKER_OPTS="--bip=10.8.0.1/24"
 EOF
 
 systemctl daemon-reload
-
-#systemctl enable docker
-#systemctl start docker
-#systemctl status docker
+systemctl enable docker
