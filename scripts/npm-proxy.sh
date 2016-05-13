88f16b4ecc600cd99794b6aa42b2134a56716bdc
Sub <subchen@gmail.com>
2016-05-13 15:14:32 +0800

    Update npm-proxy.sh

--- scripts/npm-proxy.sh
+++ scripts/npm-proxy.sh
@@ -2,14 +2,14 @@
 
 cat > /usr/bin/npm-taobao << EOF
 #!/bin/bash
-exec npm --registry=https://registry.npm.taobao.org \$*
+exec npm --registry=https://registry.npm.taobao.org "\$@"
 EOF
 
 chmod +x /usr/bin/npm-taobao
 
 cat > /usr/bin/npm-cn << EOF
 #!/bin/bash
-exec npm --registry=http://registry.cnpmjs.org \$*
+exec npm --registry=http://registry.cnpmjs.org "\$@"
 EOF
 
 chmod +x /usr/bin/npm-cn
