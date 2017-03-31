315e1446ae5aaab71166117cf68a924020fe4828
Sub <subchen@gmail.com>
2017-03-31 22:10:24 +0800

    Update dnsdock-setup.sh

--- scripts/dnsdock-setup.sh
+++ scripts/dnsdock-setup.sh
@@ -5,7 +5,7 @@ set -e
 curl https://raw.githubusercontent.com/subchen/development-favorites/master/binaries/dnsdock/dnsdock-1.12.0 -o /usr/bin/dnsdock
 chmod +x /usr/bin/dnsdock
 
-cat > /etc/systemd/system/multi-user.target.wants/dnsdock.service << EOF
+cat > /etc/systemd/system/dnsdock.service << EOF
 [Unit]
 Description=DNS service discovery for Docker containers
 Documentation=https://github.com/tonistiigi/dnsdock
