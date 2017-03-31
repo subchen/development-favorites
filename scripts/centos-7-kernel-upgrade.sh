0a3d6d3fc31ad0a1b73cd163d0c493d98b27f9de
Sub <subchen@gmail.com>
2017-03-31 21:32:42 +0800

    Update centos-7-kernel-upgrade.sh

--- scripts/centos-7-kernel-upgrade.sh
+++ scripts/centos-7-kernel-upgrade.sh
@@ -3,10 +3,10 @@
 rpm -import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 
-# kernel-lt（lt=long-term）
+# kernel-lt（lt=long-term), for centos 5,6
 # yum –enablerepo=elrepo-kernel install -y kernel-lt
 
-# kernel-ml（ml=mainline）
+# kernel-ml（ml=mainline）for centos 7
 yum --enablerepo=elrepo-kernel install -y kernel-ml
 
 echo
