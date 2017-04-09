3a8c8e371c72926880422b3307f9ce1bdac834f2
Sub <subchen@gmail.com>
2017-04-09 15:55:52 +0800

    Update get-go.sh

--- dev-box/get-go.sh
+++ dev-box/get-go.sh
@@ -5,18 +5,18 @@ set -e
 #GOLANG_VERSION=1.7.5
 #GOLANG_SHA256_SUM=2e4dd6c44f0693bef4e7b46cc701513d74c3cc44f2419bf519d7868b12931ac3
 
-GOLANG_VERSION=1.8
-GOLANG_SHA256_SUM=53ab94104ee3923e228a2cb2116e5e462ad3ebaeea06ff04463479d7f12d27ca
+GOLANG_VERSION=1.8.1
 
 GOLANG_FILENAME=go${GOLANG_VERSION}.linux-amd64.tar.gz
 GOLANG_HOME=/usr/share/go
 
 echo "Downloading $GOLANG_FILENAME ..." \
- && proxy-sh curl -fSL https://storage.googleapis.com/golang/$GOLANG_FILENAME -o $GOLANG_FILENAME \
- && echo "$GOLANG_SHA256_SUM $GOLANG_FILENAME" | sha256sum -c - \
+ && proxy-sh curl -fSL https://storage.googleapis.com/golang/${GOLANG_FILENAME} -o ${GOLANG_FILENAME} \
+ && proxy-sh curl -fSL https://storage.googleapis.com/golang/${GOLANG_FILENAME}.sha256 -o ${GOLANG_FILENAME}.sha256 \
+ && echo "$(echo ${GOLANG_FILENAME}.sha256) $GOLANG_FILENAME" | sha256sum -c - \
  && mkdir -p $GOLANG_HOME \
  && tar -zxf $GOLANG_FILENAME -C $GOLANG_HOME --strip-components=1 \
- && rm -rf $GOLANG_FILENAME
+ && rm -rf $GOLANG_FILENAME ${GOLANG_FILENAME}.sha256
 
 ln -sf $GOLANG_HOME/bin/go           /usr/local/bin/go \
  && ln -sf $GOLANG_HOME/bin/gofmt    /usr/local/bin/gofmt \
