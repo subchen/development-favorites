1a2c2caa7c4dfb23c39a17a11d3be44604dd30fe
Sub <subchen@gmail.com>
2017-04-14 11:03:49 +0800

    Update get-maven.sh

--- dev-box/get-maven.sh
+++ dev-box/get-maven.sh
@@ -3,7 +3,7 @@
 set -e
 
 MAVEN_VERSION=3.3.9
-MAVEN_HOME=/usr/share/maven
+MAVEN_HOME=/usr/local/maven
 
 echo "Downloading maven $MAVEN_VERSION ..." \
  && proxy-sh curl -fSL https://archive.apache.org/dist/maven/maven-${MAVEN_VERSION:0:1}/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz     -o maven.tar.gz \
@@ -12,5 +12,5 @@ echo "Downloading maven $MAVEN_VERSION ..." \
  && mkdir -p $MAVEN_HOME \
  && tar xzf maven.tar.gz -C $MAVEN_HOME --strip-components=1 \
  && rm -rf maven.tar.gz maven.tar.gz.md5 \
- && ln -sf $MAVEN_HOME/bin/mvn /usr/bin/mvn
+ && ln -sf $MAVEN_HOME/bin/mvn /usr/local/bin/mvn
 
