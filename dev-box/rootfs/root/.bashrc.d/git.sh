6e8fb8e1a184a591386c5eb774629e915c73819d
Sub <subchen@gmail.com>
2017-04-17 12:11:35 +0800

    Update git.sh

--- dev-box/rootfs/root/.bashrc.d/git.sh
+++ dev-box/rootfs/root/.bashrc.d/git.sh
@@ -3,5 +3,5 @@
 source /usr/local/bin/git-prompt.sh
 
 export GIT_PS1_SHOWDIRTYSTATE=1
-export PS1='[\u@\h \w]$(__git_ps1)# '
-
+#export PS1='[\u@\h \w]$(__git_ps1)# '
+export PS1='[\[\e[1;34m\]\u@\h \[\e[1;32m\]\w\[\e[m\]]\[\e[1;35m\]$(__git_ps1)\[\e[m\]# '
