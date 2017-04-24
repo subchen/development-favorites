89378fcc22f39e369103469a9e6de57439d7e9ae
Sub <subchen@gmail.com>
2017-04-24 12:28:46 +0800

    Update get-java.sh

--- dev-box/get-java.sh
+++ dev-box/get-java.sh
@@ -26,23 +26,23 @@ install_on_all_platforms() {
     JDK_SHA256_SUM=97e30203f1aef324a07c94d9d078f5d19bb6c50e638e4492722debca588210bc
     JDK_DOWNLOAD_URL=http://download.oracle.com/otn-pub/java/jdk/${JDK_VERSION}-${JDK_VERSION_PATCH}/e9e7ea248e2c4826b92b3f075a80e441/jdk-${JDK_VERSION}-linux-x64.tar.gz
 
-    JDK_HOME=/usr/local/java/jdk-${JDK_VERSION}
+    JDK_HOME=/usr/share/java/jdk-${JDK_VERSION}
 
     proxy-sh curl -fSL -H "Cookie: oraclelicense=accept-securebackup-cookie" $JDK_DOWNLOAD_URL -o jdk-linux-x64.tar.gz \
      && echo "$JDK_SHA256_SUM jdk-linux-x64.tar.gz" | sha256sum -c - \
      && mkdir -p $JDK_HOME \
      && tar -xzf jdk-linux-x64.tar.gz -C $JDK_HOME --strip-components=1 \
-     && rm -rf jdk-linux-x64.tar.gz
+     && rm -f jdk-linux-x64.tar.gz
 
-    ln -sf $JDK_HOME /usr/local/java/jdk \
-     && ln -sf /usr/local/java/jdk/bin/java      /usr/local/bin/java \
-     && ln -sf /usr/local/java/jdk/bin/javac     /usr/local/bin/javac \
-     && ln -sf /usr/local/java/jdk/bin/jar       /usr/local/bin/jar \
-     && ln -sf /usr/local/java/jdk/bin/javadoc   /usr/local/bin/javadoc \
-     && ln -sf /usr/local/java/jdk/bin/jps       /usr/local/bin/jps \
-     && ln -sf /usr/local/java/jdk/bin/jstack    /usr/local/bin/jstack \
-     && ln -sf /usr/local/java/jdk/bin/jmap      /usr/local/bin/jmap \
-     && ln -sf /usr/local/java/jdk/bin/keytool   /usr/local/bin/keytool
+    ln -sf $JDK_HOME /usr/share/java/jdk \
+     && ln -sf /usr/local/share/jdk/bin/java      /usr/local/bin/java \
+     && ln -sf /usr/local/share/jdk/bin/javac     /usr/local/bin/javac \
+     && ln -sf /usr/local/share/jdk/bin/jar       /usr/local/bin/jar \
+     && ln -sf /usr/local/share/jdk/bin/javadoc   /usr/local/bin/javadoc \
+     && ln -sf /usr/local/share/jdk/bin/jps       /usr/local/bin/jps \
+     && ln -sf /usr/local/share/jdk/bin/jstack    /usr/local/bin/jstack \
+     && ln -sf /usr/local/share/jdk/bin/jmap      /usr/local/bin/jmap \
+     && ln -sf /usr/local/share/jdk/bin/keytool   /usr/local/bin/keytool
 }
 
 install_on_all_platforms
