77dd647a9b6885de50cf30bdaf2ef0258c4fc026
Sub <subchen@gmail.com>
2017-04-24 11:34:37 +0800

    Create get-diff-so-fancy.sh

---
+++ dev-box/get-diff-so-fancy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+DSF_VERSION=0.11.4
+
+echo "Downloading diff-so-fancy-${DSF_VERSION}.tar.gz ..."
+ && proxy-sh curl -fSL https://github.com/so-fancy/diff-so-fancy/archive/v${DSF_VERSION}.tar.gz -o diff-so-fancy.tar.gz \
+ && tar -zxf diff-so-fancy.tar.gz -C /usr/local/ \
+ && rm -f diff-so-fancy.tar.gz \
+ && ln -sf /usr/local/diff-so-fancy-${DSF_VERSION}/diff-so-fancy /usr/local/bin/
+
+git config --global alias.dsf '!f() { [ -z "$GIT_PREFIX" ] || cd "$GIT_PREFIX" && git diff --color "$@" | diff-so-fancy | less --tabs=4 -RFX; }; f'
