ca514ceec1de4f7b422d621783d41480667ac95e
Sub <subchen@gmail.com>
2017-05-02 15:10:18 +0800

    Update get-java.sh

--- dev-box/get-java.sh
+++ dev-box/get-java.sh
@@ -17,22 +17,28 @@ install_on_all_platforms() {
     #JDK_VERSION_PATCH=b15
     #JDK_DOWNLOAD_URL=http://download.oracle.com/otn-pub/java/jdk/${JDK_VERSION}-${JDK_VERSION_PATCH}/jdk-${JDK_VERSION}-linux-x64.tar.gz
 
-    #JDK_VERSION=8u112
-    #JDK_VERSION_PATCH=b15
-    #JDK_DOWNLOAD_URL=http://download.oracle.com/otn-pub/java/jdk/${JDK_VERSION}-${JDK_VERSION_PATCH}/jdk-${JDK_VERSION}-linux-x64.tar.gz
+    JDK_VERSION=8u131
+    JDK_VERSION_PATCH=b11
+    JDK_DIR_HASH=d54c1d3a095b4ff2b6607d096fa80163
 
-    JDK_VERSION=8u121
-    JDK_VERSION_PATCH=b13
-    JDK_SHA256_SUM=97e30203f1aef324a07c94d9d078f5d19bb6c50e638e4492722debca588210bc
-    JDK_DOWNLOAD_URL=http://download.oracle.com/otn-pub/java/jdk/${JDK_VERSION}-${JDK_VERSION_PATCH}/e9e7ea248e2c4826b92b3f075a80e441/jdk-${JDK_VERSION}-linux-x64.tar.gz
+    case "$(uname -sm)" in
+        "Linux x86_64")
+            JDK_DOWNLOAD_URL="http://download.oracle.com/otn-pub/java/jdk/${JDK_VERSION}-${JDK_VERSION_PATCH}/${JDK_DIR_HASH}/jdk-${JDK_VERSION}-linux-x64.tar.gz"
+            JDK_SHA256_SUM=97e30203f1aef324a07c94d9d078f5d19bb6c50e638e4492722debca588210bc
+        ;;
+        "Darwin x86_64")
+            JDK_DOWNLOAD_URL="http://download.oracle.com/otn-pub/java/jdk/${JDK_VERSION}-${JDK_VERSION_PATCH}/${JDK_DIR_HASH}/jdk-${JDK_VERSION}-macosx-x64.tar.gz"
+            JDK_SHA256_SUM=1d868f8de999de7531419b7ed54b73c47923c4f5843da0341c62ee8e4a231ea9
+        ;;
+    esac
 
     JDK_HOME=/usr/share/java/jdk-${JDK_VERSION}
 
-    proxy-sh curl -fSL -H "Cookie: oraclelicense=accept-securebackup-cookie" $JDK_DOWNLOAD_URL -o jdk-linux-x64.tar.gz \
-     && echo "$JDK_SHA256_SUM jdk-linux-x64.tar.gz" | sha256sum -c - \
+    proxy-sh curl -fSL -H "Cookie: oraclelicense=accept-securebackup-cookie" $JDK_DOWNLOAD_URL -o jdk-x64.tar.gz \
+     && echo "$JDK_SHA256_SUM jdk-x64.tar.gz" | sha256sum -c - \
      && mkdir -p $JDK_HOME \
-     && tar -xzf jdk-linux-x64.tar.gz -C $JDK_HOME --strip-components=1 \
-     && rm -f jdk-linux-x64.tar.gz
+     && tar -xzf jdk-x64.tar.gz -C $JDK_HOME --strip-components=1 \
+     && rm -f jdk-x64.tar.gz
 
     ln -sf $JDK_HOME /usr/share/java/jdk \
      && ln -sf /usr/local/share/jdk/bin/java      /usr/local/bin/java \
